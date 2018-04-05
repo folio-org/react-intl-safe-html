1f370e0eb1beb46769cdf6d6fa7862f776048330
Roman Ruiz-Esparza <rjruizes@ncsu.edu>
2018-04-05 16:13:42 -0400

    tagName prop

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import sanitizeHtml from 'sanitize-html';
 
 class SafeHTMLMessage extends React.Component {
   render () {
-    const {intl, id, values} = this.props;
+    const {intl, id, values, tagName} = this.props;
     const msg = intl.formatHTMLMessage({id}, values);
-    return (<span dangerouslySetInnerHTML={{ __html: sanitizeHtml(msg) }}></span>);
+    return React.createElement(tagName || 'span', {dangerouslySetInnerHTML:{ __html: sanitizeHtml(msg)}}, null);
   }
 }
 export default injectIntl(SafeHTMLMessage);
