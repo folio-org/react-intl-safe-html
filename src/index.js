40f2be20555b86532fb4912236f474268ef9c477
Roman Ruiz-Esparza <rjruizes@ncsu.edu>
2018-04-05 15:43:59 -0400

    account for react escaping

--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@ import React from 'react';
 import { injectIntl } from 'react-intl';
 import sanitizeHtml from 'sanitize-html';
 
-function htmlDecode(input) {
-  var doc = new DOMParser().parseFromString(input, "text/html");
-  return doc.documentElement.textContent;
-}
-
 class SafeHTMLMessage extends React.Component {
   render () {
     const {intl, id, values} = this.props;
     const msg = intl.formatHTMLMessage({id}, values);
-    return (<span>{sanitizeHtml(htmlDecode(msg))}</span>);
+    return (<span dangerouslySetInnerHTML={{ __html: sanitizeHtml(msg) }}></span>);
   }
 }
 export default injectIntl(SafeHTMLMessage);
